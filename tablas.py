bc1156d9c090e5b63742e15aca1e6fe8a75c8122
Isaac Lopez <isaaclpe@gmail.com>
2012-11-18 17:32:16 -0430

    añadida entrada

---
+++ tablas.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Hecho Por:
+# Isaac López Procopio
+# 07-41120
+# Juan V. Rosas
+# 07-41502
+# Hancel L. Gonzalez
+# Bla
+
+def main():
+    disitintos = []
+    for celda in range(81):
+        distintos = distintos + []
+        fila = celda % 9
+        columna = celda / 9
+        for comparando in range (81):
+            if comparando % 9 == fila:
+                distintos
+    
+main()
