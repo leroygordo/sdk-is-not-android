8478e6b901daddac70c0952bafef38a141124fbe
Isaac Lopez <isaaclpe@gmail.com>
2012-11-18 18:37:44 -0430

    tabla generada

--- tablas.py
+++ tablas.py
@@ -8,14 +8,42 @@
 # Hancel L. Gonzalez
 # Bla
 
+def fila(a):
+    return a % 9
+    
+def columna(a):
+    return a / 9
+    
+def cuadro_H(a):
+    return fila(a) / 3
+    
+def cuadro_V(a):
+    return columna(a) / 3
+    
+def mismo_cuadro(a, b, c):
+    return a == cuadro_H(c) and b == cuadro_V(c)
+    
+    
+
 def main():
-    disitintos = []
+
+    distintos = []
     for celda in range(81):
-        distintos = distintos + []
-        fila = celda % 9
-        columna = celda / 9
+        actual = []
+        celda_fila = fila(celda)
+        celda_columna = columna(celda)
+        celda_cuadro_H = cuadro_H(celda)
+        celda_cuadro_V = cuadro_V(celda)
         for comparando in range (81):
-            if comparando % 9 == fila:
-                distintos
+            if celda != comparando and \
+            (fila(comparando) == celda_fila or \
+            columna(comparando) == celda_columna or \
+            mismo_cuadro(celda_cuadro_H, celda_cuadro_V, comparando)) :
+                actual = actual + [comparando]
+        distintos = distintos + [actual]
+    
+    print distintos
+#    for i in range(81):
+#        print i , distintos[i]
     
 main()
